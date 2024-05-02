f5444317602b47a8e6240646deb9903af4fbd7a8
Lentement26 <minhbui261103.com>
2024-05-02 11:07:35 +0700

    python

--- demo_lcd.py
+++ demo_lcd.py
@@ -11,15 +11,15 @@ try:
     while True:
         # Remember that your sentences can only be 16 characters long!
         print("Writing to display")  # In ấn một thông báo chỉ định việc ghi vào màn hình
-        display.lcd_display_string("Greetings Human!", 1)  # Viết dòng văn bản lên dòng đầu tiên của màn hình
-        display.lcd_display_string("Demo Pi Guy code", 2)  # Viết dòng văn bản lên dòng thứ hai của màn hình
+        display.lcd_display_string("G", 1)  # Viết dòng văn bản lên dòng đầu tiên của màn hình
+        display.lcd_display_string("D", 2)  # Viết dòng văn bản lên dòng thứ hai của màn hình
         sleep(2)  # Chờ một khoảng thời gian để đọc tin nhắn
-        display.lcd_display_string("I am a display!", 1)   # Làm mới dòng đầu tiên của màn hình với một thông điệp khác
+        display.lcd_display_string("I", 1)   # Làm mới dòng đầu tiên của màn hình với một thông điệp khác
         sleep(2)  # Chờ một khoảng thời gian để đọc tin nhắn
         display.lcd_clear()  # Xóa màn hình khỏi bất kỳ dữ liệu nào
         sleep(2)  # Chờ một khoảng thời gian để đọc tin nhắn
 except KeyboardInterrupt:
     # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
-    print("Cleaning up!")  # In ấn một thông báo chỉ định việc dọn dẹp
+    print("Clean!")  # In ấn một thông báo chỉ định việc dọn dẹp
     display.lcd_clear()  # Xóa màn hình
 
